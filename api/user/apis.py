a9c0361b87bcebba62577b02be15b4a1cd7c9b6e
monyorojoseph <monyorojoseph@gmail.com>
2022-09-07 00:15:57 +0300

    html and js no web bundler

--- api/user/apis.py
+++ api/user/apis.py
@@ -32,7 +32,8 @@ RP_NAME = "Example Co"
 """
 class UserAPI(viewsets.ModelViewSet):
     serializer_class = UserSerializer
-
+    
+    # registration options
     @action(detail=True, methods=['POST'])
     def registration(self, request, format=None):
         data = json.loads(request.body)
@@ -52,3 +53,40 @@ class UserAPI(viewsets.ModelViewSet):
         )
         data = json.loads(options_to_json(options))
         return Response(data, status=status.HTTP_201_CREATED)
+    
+    # verify registration response
+    @action(detail=True, methods=['POST'])
+    def registration_verification(self, request, format=None):
+        pass
+        # pregistration_verification = verify_registration_response(
+        # credential=RegistrationCredential.parse_raw(
+        #     """{
+        #     "id": "ZoIKP1JQvKdrYj1bTUPJ2eTUsbLeFkv-X5xJQNr4k6s",
+        #     "rawId": "ZoIKP1JQvKdrYj1bTUPJ2eTUsbLeFkv-X5xJQNr4k6s",
+        #     "response": {
+        #         "attestationObject": "o2NmbXRkbm9uZWdhdHRTdG10oGhhdXRoRGF0YVkBZ0mWDeWIDoxodDQXD2R2YFuP5K65ooYyx5lc87qDHZdjRQAAAAAAAAAAAAAAAAAAAAAAAAAAACBmggo_UlC8p2tiPVtNQ8nZ5NSxst4WS_5fnElA2viTq6QBAwM5AQAgWQEA31dtHqc70D_h7XHQ6V_nBs3Tscu91kBL7FOw56_VFiaKYRH6Z4KLr4J0S12hFJ_3fBxpKfxyMfK66ZMeAVbOl_wemY4S5Xs4yHSWy21Xm_dgWhLJjZ9R1tjfV49kDPHB_ssdvP7wo3_NmoUPYMgK-edgZ_ehttp_I6hUUCnVaTvn_m76b2j9yEPReSwl-wlGsabYG6INUhTuhSOqG-UpVVQdNJVV7GmIPHCA2cQpJBDZBohT4MBGme_feUgm4sgqVCWzKk6CzIKIz5AIVnspLbu05SulAVnSTB3NxTwCLNJR_9v9oSkvphiNbmQBVQH1tV_psyi9HM1Jtj9VJVKMeyFDAQAB",
+        #         "clientDataJSON": "eyJ0eXBlIjoid2ViYXV0aG4uY3JlYXRlIiwiY2hhbGxlbmdlIjoiQ2VUV29nbWcwY2NodWlZdUZydjhEWFhkTVpTSVFSVlpKT2dhX3hheVZWRWNCajBDdzN5NzN5aEQ0RmtHU2UtUnJQNmhQSkpBSW0zTFZpZW40aFhFTGciLCJvcmlnaW4iOiJodHRwOi8vbG9jYWxob3N0OjUwMDAiLCJjcm9zc09yaWdpbiI6ZmFsc2V9"
+        #     },
+        #     "type": "public-key",
+        #     "clientExtensionResults": {},
+        #     "transports": ["internal"]
+        # }"""
+        # ),
+        # expected_challenge=base64url_to_bytes(
+        #     "CeTWogmg0cchuiYuFrv8DXXdMZSIQRVZJOga_xayVVEcBj0Cw3y73yhD4FkGSe-RrP6hPJJAIm3LVien4hXELg"
+        # ),
+        # expected_origin="http://localhost:5000",
+        # expected_rp_id="localhost",
+        # require_user_verification=True,
+    # )
+
+    # authentication options
+    @action(detail=True, methods=['POST'])
+    def authentication(self, request, format=None):
+        pass
+
+    # verify authentication
+    @action(detail=True, methods=['POST'])
+    def authentication_verification(self, request, format=None):
+        pass
+    
