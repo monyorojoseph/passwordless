eb9abb3fa451d56b49b03f4e65882c0bec75bb4d
monyorojoseph <monyorojoseph@gmail.com>
2022-09-07 02:30:01 +0300

    changes on api for verification in registration and authentication

--- api/user/apis.py
+++ api/user/apis.py
@@ -8,26 +8,32 @@ from webauthn import (
     verify_registration_response,
     options_to_json,
     base64url_to_bytes,
+    generate_authentication_options,
+    verify_authentication_response,
 )
 from webauthn.helpers.cose import COSEAlgorithmIdentifier
 from webauthn.helpers.structs import (
     AttestationConveyancePreference,
     AuthenticatorAttachment,
     AuthenticatorSelectionCriteria,
-    PublicKeyCredentialDescriptor,
     ResidentKeyRequirement,
     RegistrationCredential,
+    UserVerificationRequirement,
+    AuthenticationCredential,
 )
 from .serializers import UserSerializer
 
 User = get_user_model()
 
-RP_ID = "monyorojoseph.herokuapp.com"
-RP_NAME = "MJ co"
+RP_ID = "bioemtric-auth.herokuapp.com"
+RP_NAME = "Webauthn Webauthn"
+ORGIN = "https://bioemtric-auth.herokuapp.com"
+
 
 # Create your views here.
 """
     1. Registration
+    2. Authentcation
 
 """
 class UserAPI(viewsets.ModelViewSet):
@@ -58,36 +64,45 @@ class UserAPI(viewsets.ModelViewSet):
     # verify registration response
     @action(detail=True, methods=['POST'])
     def registration_verification(self, request, format=None):
-        pass
-        # pregistration_verification = verify_registration_response(
-        # credential=RegistrationCredential.parse_raw(
-        #     """{
-        #     "id": "ZoIKP1JQvKdrYj1bTUPJ2eTUsbLeFkv-X5xJQNr4k6s",
-        #     "rawId": "ZoIKP1JQvKdrYj1bTUPJ2eTUsbLeFkv-X5xJQNr4k6s",
-        #     "response": {
-        #         "attestationObject": "o2NmbXRkbm9uZWdhdHRTdG10oGhhdXRoRGF0YVkBZ0mWDeWIDoxodDQXD2R2YFuP5K65ooYyx5lc87qDHZdjRQAAAAAAAAAAAAAAAAAAAAAAAAAAACBmggo_UlC8p2tiPVtNQ8nZ5NSxst4WS_5fnElA2viTq6QBAwM5AQAgWQEA31dtHqc70D_h7XHQ6V_nBs3Tscu91kBL7FOw56_VFiaKYRH6Z4KLr4J0S12hFJ_3fBxpKfxyMfK66ZMeAVbOl_wemY4S5Xs4yHSWy21Xm_dgWhLJjZ9R1tjfV49kDPHB_ssdvP7wo3_NmoUPYMgK-edgZ_ehttp_I6hUUCnVaTvn_m76b2j9yEPReSwl-wlGsabYG6INUhTuhSOqG-UpVVQdNJVV7GmIPHCA2cQpJBDZBohT4MBGme_feUgm4sgqVCWzKk6CzIKIz5AIVnspLbu05SulAVnSTB3NxTwCLNJR_9v9oSkvphiNbmQBVQH1tV_psyi9HM1Jtj9VJVKMeyFDAQAB",
-        #         "clientDataJSON": "eyJ0eXBlIjoid2ViYXV0aG4uY3JlYXRlIiwiY2hhbGxlbmdlIjoiQ2VUV29nbWcwY2NodWlZdUZydjhEWFhkTVpTSVFSVlpKT2dhX3hheVZWRWNCajBDdzN5NzN5aEQ0RmtHU2UtUnJQNmhQSkpBSW0zTFZpZW40aFhFTGciLCJvcmlnaW4iOiJodHRwOi8vbG9jYWxob3N0OjUwMDAiLCJjcm9zc09yaWdpbiI6ZmFsc2V9"
-        #     },
-        #     "type": "public-key",
-        #     "clientExtensionResults": {},
-        #     "transports": ["internal"]
-        # }"""
-        # ),
-        # expected_challenge=base64url_to_bytes(
-        #     "CeTWogmg0cchuiYuFrv8DXXdMZSIQRVZJOga_xayVVEcBj0Cw3y73yhD4FkGSe-RrP6hPJJAIm3LVien4hXELg"
-        # ),
-        # expected_origin="http://localhost:5000",
-        # expected_rp_id="localhost",
-        # require_user_verification=True,
-    # )
+        registration_verification = verify_registration_response(
+            credential=RegistrationCredential.parse_raw(request.data),
+            expected_challenge=base64url_to_bytes(
+                "CeTWogmg0cchuiYuFrv8DXXdMZSIQRVZJOga_xayVVEcBj0Cw3y73yhD4FkGSe-RrP6hPJJAIm3LVien4hXELg"
+            ),
+            expected_origin=ORGIN,
+            expected_rp_id=RP_ID,
+            require_user_verification=True,
+        )
+
+        data = json.loads(registration_verification.json(indent=2))
+        return Response(data, status=status.HTTP_200_OK)
 
     # authentication options
     @action(detail=True, methods=['POST'])
     def authentication(self, request, format=None):
-        pass
+        options = generate_authentication_options(
+            rp_id=RP_ID,
+            user_verification=UserVerificationRequirement.REQUIRED,
+        )
+        data = json.loads(options_to_json(options))
+        return Response(data, status=status.HTTP_200_OK)
 
     # verify authentication
     @action(detail=True, methods=['POST'])
     def authentication_verification(self, request, format=None):
-        pass
+        authentication_verification = verify_authentication_response(
+            credential=AuthenticationCredential.parse_raw(request.data),
+            expected_challenge=base64url_to_bytes(
+                "iPmAi1Pp1XL6oAgq3PWZtZPnZa1zFUDoGbaQ0_KvVG1lF2s3Rt_3o4uSzccy0tmcTIpTTT4BU1T-I4maavndjQ"
+            ),
+            expected_rp_id=RP_ID,
+            expected_origin=ORGIN,
+            credential_public_key=base64url_to_bytes(
+                "pAEDAzkBACBZAQDfV20epzvQP-HtcdDpX-cGzdOxy73WQEvsU7Dnr9UWJophEfpngouvgnRLXaEUn_d8HGkp_HIx8rrpkx4BVs6X_B6ZjhLlezjIdJbLbVeb92BaEsmNn1HW2N9Xj2QM8cH-yx28_vCjf82ahQ9gyAr552Bn96G22n8jqFRQKdVpO-f-bvpvaP3IQ9F5LCX7CUaxptgbog1SFO6FI6ob5SlVVB00lVXsaYg8cIDZxCkkENkGiFPgwEaZ7995SCbiyCpUJbMqToLMgojPkAhWeyktu7TlK6UBWdJMHc3FPAIs0lH_2_2hKS-mGI1uZAFVAfW1X-mzKL0czUm2P1UlUox7IUMBAAE"
+            ),
+            credential_current_sign_count=0,
+            require_user_verification=True,
+        )
+        data = json.loads(authentication_verification.json(indent=2))
+        return Response(data, status=status.HTTP_200_OK)
     
