623ace294a632247ff0cb6a7fca79238bf5fdf24
monyorojoseph <monyorojoseph@gmail.com>
2022-09-07 12:20:44 +0300

    Prepped for deployment

--- api/setup/settings.py
+++ api/setup/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 from decouple import config
 import ast
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -49,6 +50,7 @@ USER_AUTH_MODEL = 'user.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
@@ -81,17 +83,20 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': config('SQL_ENGINE'),
-        'NAME': config('SQL_DB'),
-        'USER': config('SQL_USER'),
-        'PASSWORD': config('SQL_PASS'),
-        'PORT': config('SQL_PORT'),
-        'HOST': config('SQL_HOST')
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': config('SQL_ENGINE'),
+            'NAME': config('SQL_DB'),
+            'USER': config('SQL_USER'),
+            'PASSWORD': config('SQL_PASS'),
+            'PORT': config('SQL_PORT'),
+            'HOST': config('SQL_HOST')
+        }
     }
-}
+else:
+    DATABASES = {'default': dj_database_url.config(conn_max_age=600)}
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 # Password validation
@@ -116,7 +121,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'Africa/Nairobi'
 
 TIME_ZONE = 'UTC'
 
@@ -128,7 +133,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = 'static/'
+
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
