6da74efcb025c4e9600618680c71f0df423ecef3
SushantDEV23 <92986663+SushantDEV23@users.noreply.github.com>
2022-09-08 20:20:12 +0530

    Delete api directory

--- api/authentication/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import *
-from django.contrib.auth.models import User
-from dataclasses import fields
-class UserSerializers(serializers.ModelSerializer):
-    class Meta:
-        model=User
-        fields=['username', 'password']
