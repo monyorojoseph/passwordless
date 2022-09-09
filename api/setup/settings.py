cd331dfa3a6a31cec352d64f2870155744ae333a
monyorojoseph <monyorojoseph@gmail.com>
2022-09-09 13:59:09 +0300

    Change static storage

--- api/setup/settings.py
+++ api/setup/settings.py
@@ -135,8 +135,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
-
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
